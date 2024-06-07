33c80068aad69688a0208af39698760b2663a4f8
Quân Đinh <quan.dnl@icloud.com>
2024-06-07 14:03:42 +0000

    buuble sort

--- Start/Sorting/BubbleSort/bubble_start.py
+++ Start/Sorting/BubbleSort/bubble_start.py
@@ -3,9 +3,15 @@
 
 
 def bubbleSort(dataset):
-    # TODO: start with the array length and decrement each time
-
-    print("Current state: ", dataset)
+    # start with the array length and decrement each time
+    for i in range(len(dataset)-1, 0, -1):
+        for j in range(i):
+            if dataset[j] > dataset[j + 1]:
+                temp = dataset[j]
+                # swapping positions
+                dataset[j] = dataset[j + 1]
+                dataset[j + 1] = temp
+        print("Current state: ", dataset)
 
 
 list1 = [6, 20, 8, 19, 56, 23, 87, 41, 49, 53]
