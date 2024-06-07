b1bc8bd7ccf95280468167fe14530cc4a626b480
Quân Đinh <quan.dnl@icloud.com>
2024-06-07 11:18:02 +0000

    recursive

--- Start/Recursion/countdown_start.py
+++ Start/Recursion/countdown_start.py
@@ -3,7 +3,12 @@
 
 
 def countdown(x):
-    return
+    if x == 0: 
+        print("Done!")
+        return
+    else:
+        print(x, "...")
+        countdown(x - 1)
 
 
 countdown(5)
--- Start/Recursion/recursion_start.py
+++ Start/Recursion/recursion_start.py
@@ -3,12 +3,14 @@
 
 # 2^4 = 2*2*2*2 = 16
 def power(num, pwr):
-    pass
+    if pwr == 0: return 1
+    else: return num * power(num, pwr-1)
 
 # 5! = 5*4*3*2*1
 # Special case: 0! is 1, because... math
 def factorial(num):
-    pass
+    if num == 0: return 1
+    else: return num * factorial(num - 1)
 
 
 print(f"5 to the power of 3 is {power(5, 3)}")
