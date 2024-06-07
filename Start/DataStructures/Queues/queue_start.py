75508a2a1f8fc7b7f76f276c3a64b7f5056d8cd7
Quân Đinh <quan.dnl@icloud.com>
2024-06-07 10:20:26 +0000

    stack & queue

--- Start/DataStructures/Queues/queue_start.py
+++ Start/DataStructures/Queues/queue_start.py
@@ -1,14 +1,21 @@
 # Example file for Programming Foundations: Algorithms by Joe Marini
 # try out the Python queue functions
-
+from collections import deque
 
 # TODO: create a new empty deque object that will function as a queue
-
+queue = deque()
 
 # TODO: add some items to the queue
-
+queue.append(1)
+queue.append(2)
+queue.append(3)
+queue.append(4)
 
 # TODO: print the queue contents
-
+print(queue)
 
 # TODO: pop an item off the front of the queue
+queue.popleft()
+print(queue)
+queue.pop()
+print(queue)
